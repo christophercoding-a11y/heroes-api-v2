cb2519072b97eda7df7d0eb180e84294f1503210
christophercodinga11y <christopherjack14@gmail.com>
2025-04-21 13:50:10 -0500

    update

--- app/daos/api/heroDao.js
+++ app/daos/api/heroDao.js
@@ -42,7 +42,13 @@ const heroDao = {
                         powers.push(obj.power)
                     })
                     con.execute(
-                        `SELECT h1.hero_name hero, h2.hero_name rival
+                        `SELECT h1.hero_name hero, 
+                        case when h1.hero_name is null then concat(h1.first_name, ' ', h1.last_name)
+                        else h1.hero_name
+                        end hero,
+                        case when h2.hero_name is null then concat(h2.first_name, ' ', h2.last_name)
+                        else h2.hero_name
+                        end rival
                         FROM hero_to_rival hr 
                         JOIN hero h1 ON h1.hero_id = hr.hero_id
                         JOIN hero h2 ON h2.hero_id = hr.rival_id
